ec051de88f0e2d477e82804d08c8a7f434c257d0
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2024-11-20 14:33:30 +0500

    Huge Refactoring

--- src/views/Facts.vue
+++ src/views/Facts.vue
@@ -68,7 +68,7 @@
         <div class="buttons d-flex gap-3 align-items-center">
           <a
             v-if="selectValue === 'facts/'"
-            :href="`https://openinfo.uz/ru/facts/to_pdf/${fact.id}`"
+            :href="`${downloadUrl}/facts/to_pdf/${fact.id}`"
           >
             <img
               style="width: 26px"
@@ -78,7 +78,7 @@
           </a>
           <a
             v-if="selectValue !== 'facts/'"
-            :href="`https://openinfo.uz/ru/facts/go_pdf/?fact=${fact.fact_number}&id=${fact.id}`"
+            :href="`${downloadUrl}/facts/go_pdf/?fact=${fact.fact_number}&id=${fact.id}`"
           >
             <img
               style="width: 26px"
@@ -159,171 +159,172 @@
 </template>
 
 <script setup>
-  import { computed, onMounted, ref } from 'vue'
-  import axios from 'axios'
-  import { Calendar, Clock, Download, Search } from '@element-plus/icons-vue'
-  import { ElNotification } from 'element-plus'
+import { computed, onMounted, ref } from 'vue';
+import axios from 'axios';
+import { Calendar, Clock, Download, Search } from '@element-plus/icons-vue';
+import { ElNotification } from 'element-plus';
 
-  import moment from 'moment'
+import moment from 'moment';
 
-  import { useRoute, useRouter } from 'vue-router'
-  import options from '../components/options'
-  import { useStore } from '@/stores/store'
+import { useRoute, useRouter } from 'vue-router';
+import options from '../components/options';
+import { useStore } from '@/stores/store';
 
-  const factLists = ref([])
+const factLists = ref([]);
 
-  const url = import.meta.env.VITE_APP_SERVER_URL
+const url = import.meta.env.VITE_APP_SERVER_URL;
+const downloadUrl = import.meta.env.VITE_APP_DOWNLOAD_URL;
 
-  const dateValue = ref(null)
-  const route = useRoute()
-  const router = useRouter()
-  const selectValue = ref(options[0].value)
-  const loading = ref(false)
+const dateValue = ref(null);
+const route = useRoute();
+const router = useRouter();
+const selectValue = ref(options[0].value);
+const loading = ref(false);
 
-  const totalPages = ref(null)
-  const total = ref(null)
+const totalPages = ref(null);
+const total = ref(null);
 
-  const results = ref([])
-  const factNumber = ref(1)
+const results = ref([]);
+const factNumber = ref(1);
 
-  const factSearchInput = ref('')
-  const suggestionsList = ref([])
+const factSearchInput = ref('');
+const suggestionsList = ref([]);
 
-  const selectChange = () => {
-    currentPage.value = 1
-    getFacts()
-  }
+const selectChange = () => {
+  currentPage.value = 1;
+  getFacts();
+};
 
-  const store = useStore()
+const store = useStore();
 
-  const querySearch = (queryString, cb) => {
-    axios
-      .get(`${url}/home/autofill/?name=${queryString}`)
-      .then((response) => {
-        suggestionsList.value = response.data
+const querySearch = (queryString, cb) => {
+  axios
+    .get(`${url}/home/autofill/?name=${queryString}`)
+    .then((response) => {
+      suggestionsList.value = response.data;
 
-        // call callback function to return suggestions
-        cb(suggestionsList.value)
-      })
-      .catch((err) => {
-        console.log(err, 'error')
-      })
-  }
+      // call callback function to return suggestions
+      cb(suggestionsList.value);
+    })
+    .catch((err) => {
+      console.log(err, 'error');
+    });
+};
 
-  const getFacts = () => {
-    loading.value = true
-    axios
-      .get(
-        `${url}/disclosure/${selectValue?.value ? selectValue?.value : 'facts/'}?page=${currentPage.value}${dateValue.value?.length ? `&pub_date__lte=${(dateValue.value?.[1]).toISOString()}&pub_date__gte=${moment(dateValue.value?.[0]).toISOString()}` : ''}&search=${factSearchInput.value}`,
-        {
-          method: 'get',
-        },
-      )
-      .then((response) => {
-        factLists.value = response.data.results
-        totalPages.value = response.data.total_pages
-        total.value = response.data.count
-      })
-      .catch(({ response }) => {
-        ElNotification({
-          type: 'error',
-          message: response.data.message || 'Malumot olishda xatolik bor!',
-        })
-      })
-      .finally(() => {
-        loading.value = false
-      })
-  }
+const getFacts = () => {
+  loading.value = true;
+  axios
+    .get(
+      `${url}/disclosure/${selectValue?.value ? selectValue?.value : 'facts/'}?page=${currentPage.value}${dateValue.value?.length ? `&pub_date__lte=${(dateValue.value?.[1]).toISOString()}&pub_date__gte=${moment(dateValue.value?.[0]).toISOString()}` : ''}&search=${factSearchInput.value}`,
+      {
+        method: 'get',
+      },
+    )
+    .then((response) => {
+      factLists.value = response.data.results;
+      totalPages.value = response.data.total_pages;
+      total.value = response.data.count;
+    })
+    .catch(({ response }) => {
+      ElNotification({
+        type: 'error',
+        message: response.data.message || 'Malumot olishda xatolik bor!',
+      });
+    })
+    .finally(() => {
+      loading.value = false;
+    });
+};
 
-  // const getFactsBySearch = () => {
-  //   loading.value = true
-  //   axios.get(`${url}/disclosure/${selectValue.value}?page=${currentPage.value}${dateValue.value?.length ? `&pub_date__lte=${(dateValue.value?.[1]).toISOString()}&pub_date__gte=${moment(dateValue.value?.[0]).toISOString()}` : ''}&search=${factSearchInput.value}`, {
-  //     method: 'get',
-  //   })
-  //       .then(response => {
-  //         factLists.value = response.data.results
-  //         totalPages.value = response.data.total_pages
-  //         total.value = response.data.count
-  //
-  //       })
-  //       .catch(({response}) => {
-  //         ElNotification({
-  //           type: "error",
-  //           message: response.data.message || "Malumot olishda xatolik bor!"
-  //         })
-  //       }).finally(() => {
-  //     loading.value = false
-  //
-  //   })
-  // }
+// const getFactsBySearch = () => {
+//   loading.value = true
+//   axios.get(`${url}/disclosure/${selectValue.value}?page=${currentPage.value}${dateValue.value?.length ? `&pub_date__lte=${(dateValue.value?.[1]).toISOString()}&pub_date__gte=${moment(dateValue.value?.[0]).toISOString()}` : ''}&search=${factSearchInput.value}`, {
+//     method: 'get',
+//   })
+//       .then(response => {
+//         factLists.value = response.data.results
+//         totalPages.value = response.data.total_pages
+//         total.value = response.data.count
+//
+//       })
+//       .catch(({response}) => {
+//         ElNotification({
+//           type: "error",
+//           message: response.data.message || "Malumot olishda xatolik bor!"
+//         })
+//       }).finally(() => {
+//     loading.value = false
+//
+//   })
+// }
 
-  const clearFields = () => {
-    factSearchInput.value = ''
-    dateValue.value = []
-    selectValue.value = options[0].value
-  }
+const clearFields = () => {
+  factSearchInput.value = '';
+  dateValue.value = [];
+  selectValue.value = options[0].value;
+};
 
-  const currentPage = ref(1)
-  const handleCurrentChange = (val) => {
-    currentPage.value = val
-    getFacts()
-  }
+const currentPage = ref(1);
+const handleCurrentChange = (val) => {
+  currentPage.value = val;
+  getFacts();
+};
 
-  onMounted(() => {
-    getFacts()
-  })
+onMounted(() => {
+  getFacts();
+});
 </script>
 
 <style scoped>
-  .fact-container {
-    min-height: 80vh;
-  }
+.fact-container {
+  min-height: 80vh;
+}
 
-  .header-name {
-    /* background: #124483; */
-    border-radius: 10px;
-    text-transform: uppercase;
-    color: #fff;
-    text-align: center;
-    height: 50px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: #124483;
-    gap: 20px;
-  }
+.header-name {
+  /* background: #124483; */
+  border-radius: 10px;
+  text-transform: uppercase;
+  color: #fff;
+  text-align: center;
+  height: 50px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #124483;
+  gap: 20px;
+}
 
-  .facts-list {
-    display: flex;
-    color: #5c5c5c;
-    font-size: 16px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 1.4;
-    flex-direction: column;
-    gap: 10px;
-    margin: 20px 0;
-    padding-bottom: 10px;
-    border-bottom: 1px solid #ddd;
-  }
+.facts-list {
+  display: flex;
+  color: #5c5c5c;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 1.4;
+  flex-direction: column;
+  gap: 10px;
+  margin: 20px 0;
+  padding-bottom: 10px;
+  border-bottom: 1px solid #ddd;
+}
 
-  .organization-in-facts {
-    margin: 0;
-    color: #124483;
-    font-size: 20px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: normal;
-  }
+.organization-in-facts {
+  margin: 0;
+  color: #124483;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+}
 
-  .search-bar {
-    display: flex;
-    margin: 20px 0;
-    gap: 10px;
-  }
+.search-bar {
+  display: flex;
+  margin: 20px 0;
+  gap: 10px;
+}
 
-  .fact-date {
-    font-size: 14px;
-  }
+.fact-date {
+  font-size: 14px;
+}
 </style>
